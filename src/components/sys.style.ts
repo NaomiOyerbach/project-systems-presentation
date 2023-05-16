26210d03c3deac34edf26e49a75a8d6dced7c742
NaomiAuerbach <nomio30480@gmail.com>
2023-05-17 00:03:39 +0300

    styles

---
+++ src/components/sys.style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const SystemCardStyle = styled.div`
+    padding: 80px;
+    margin: 15px;
+    // display: flex;
+    border: 3px solid black;
+    width: 200px;
+    height:280px;
+`;
